127a599d65bd10d4d2cdfd98c7fbf3f4b47e795e
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-02-26 14:46:12 -0600

    added displaying animals and locations exercise

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,16 +1,53 @@
-import React, { Component } from 'react'
-import EmployeeList from "./employee/EmployeeList"  // Import EmployeeList component
-import LocationList from './location/LocationList';
+import React, { Component } from "react"
+import EmployeeList from "./employee/EmployeeList"
+import LocationList from "./location/LocationList"
+import AnimalList from "./animals/AnimalList"
+import "./Kennel.css"
 
 
-export default class Kennel extends Component {
+class Kennel extends Component {
+
+    /*
+        Although you will eventually be pulling your objects
+        from your json-server API, for this chapter, we're
+        faking it and just creating those arrays in the component
+        itself
+    */
+    employeesFromAPI = [
+        { id: 1, name: "Jessica Younker" },
+        { id: 2, name: "Jordan Nelson" },
+        { id: 3, name: "Zoe LeBlanc" },
+        { id: 4, name: "Blaise Roberts" }
+    ]
+    locationsFromAPI = [
+        { id: 1, name: "Nashville North", address: "500 Circle Way" },
+        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
+    ]
+    animalsFromAPI = [
+        { id: 1, name: "Biggy" },
+        { id: 2, name: "Pac" },
+        { id: 3, name: "Spot" },
+        { id: 4, name: "Doggy" }
+    ]
+
+    // This will eventually get pulled from the API
+
+    state = {
+        employees: this.employeesFromAPI,
+        locations: this.locationsFromAPI,
+        animals: this.animalsFromAPI
+
+    }
+
     render() {
         return (
-            <div>
-                <h3>Student Kennels</h3>
-                <LocationList />
-                <EmployeeList />
-            </div>
-        );
+            <article className="kennel">
+                <EmployeeList employees={this.state.employees} />
+                <LocationList locations={this.state.locations} />
+                <AnimalList animals={this.state.animals} />
+            </article>
+        )
     }
 }
+
+export default Kennel
